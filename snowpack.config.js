7a7c64307a27ba32569c72236ecb511f0a7accb4
Gael <gael@shippabo.com>
2021-04-01 16:18:28 -0700

    Proxy example

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,5 @@
+const proxy = require('http2-proxy');
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
@@ -8,20 +10,19 @@ module.exports = {
     /* ... */
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {
+      src: '/api/.*',
+      dest: (req, res) => {
+        req.url = req.url.replace(/^\/api/, '');
+
+        return proxy.web(req, res, {
+          hostname: 'localhost',
+          port: 3001,
+        });
+      },
+    },
   ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
   devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
+    secure: true,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './css/main.css';
+fetch('/api/hello.json')
+  .then((res) => res.json())
+  .then(
+    (res) =>
+      (document.getElementById('main').innerHTML = JSON.stringify(
+        { res },
+        null,
+        4,
+      )),
+  );
